e1f65b29f1b03964ad077bbc3f06db29250c6cad
zidantech <zidantech4@gmail.com>
2023-05-30 11:16:22 +0100

    s

---
+++ Fandom/js/main.js
@@ -0,0 +1,18 @@
+// Slide Show Script
+
+// var slideIndex = 0;
+// showSlides();
+
+// function showSlides(){
+//     var i;
+//     var slide = document.getElementsByClassName("slides");
+
+//     for (i = 0; i < slide.length; i++){
+//         slide[i].style.display = "none";
+//     }
+//     slideIndex++;
+    
+//     if (slideIndex > slide.length){ slideIndex = 1};
+//     slide[slideIndex - 1].style.display = "block";
+//     setTimeout(showSlides, 3000)
+// }
